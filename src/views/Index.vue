d645c9fab190f0e23a9f4f98cb6e51a414aa7860
suveng <suveng@163.com>
2019-12-01 13:52:42 +0800

    求助智样:回到顶部

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,12 +1,70 @@
 <template>
     <div class="index">
-        初始化页面
+        <el-container>
+            <el-aside>
+                <el-row>
+                    <el-link>项目一览<i class="el-icon-view el-icon--right"></i></el-link>
+                </el-row>
+                <el-row>
+                    <el-link>联 系 我<i class="el-icon-view el-icon--right"></i></el-link>
+                </el-row>
+                <el-row>
+                    <el-link>技术介绍<i class="el-icon-view el-icon--right"></i></el-link>
+                </el-row>
+                <el-row>
+                    <el-link>工作历程<i class="el-icon-view el-icon--right"></i></el-link>
+                </el-row>
+            </el-aside>
+
+            <el-container>
+                <el-main>
+                    <el-row>row</el-row>
+                    <el-row>row</el-row>
+                    <el-row>row</el-row>
+                    <el-row>row</el-row>
+                    <el-row>row</el-row>
+                    <el-row>row</el-row>
+                    <el-row>row</el-row>
+                    <el-row>row</el-row>
+                    <el-row>row</el-row>
+                    <el-row>row</el-row>
+                    <el-row>row</el-row>
+                    <el-row>row</el-row>
+                    <el-row>row</el-row>
+                    <el-row>row</el-row>
+                </el-main>
+            </el-container>
+
+        </el-container>
+
     </div>
 </template>
 
 <script>
     export default {
         name: "index",
-        components:{}
+        components: {}
     }
 </script>
+
+<style scoped>
+
+    .index {
+        min-width: 100%;
+        min-height: 100%;
+    }
+
+    .el-aside {
+        background-color: #D3DCE6;
+        color: #333;
+        text-align: center;
+        line-height: 100px;
+    }
+
+    .el-main {
+        background-color: #E9EEF3;
+        color: #333;
+        text-align: center;
+        line-height: 160px;
+    }
+</style>
