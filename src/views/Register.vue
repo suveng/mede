871ef078e53c7b8066cb7ee2f50c9a96710e573b
suveng <suveng@163.com>
2019-12-02 19:54:23 +0800

    简单注册页面

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,11 +1,18 @@
 <template>
     <div class="register">
-        <el-form ref="form" :model="registerUser"  label-width="80px" class="registerForm">
+        <el-form :model="registerUser" :rules="rules" label-width="80px" class="registerForm" ref="registerForm">
             <el-form-item label="用户名" prop="username">
-                <el-input v-model="registerUser.username" placeholder="请输入用户名"/>
+                <el-input class="text-input" v-model="registerUser.username" placeholder="请输入用户名"/>
             </el-form-item>
             <el-form-item label="密码" prop="password">
-                <el-input v-model="registerUser.password" placeholder="请输入密码"/>
+                <el-input class="text-input" v-model="registerUser.password" placeholder="请输入密码"/>
+            </el-form-item>
+            <el-form-item label="重复密码" prop="repeatPassword">
+                <el-input class="text-input" v-model="registerUser.repeatPassword" placeholder="请输入密码"/>
+            </el-form-item>
+
+            <el-form-item>
+                <el-button type="primary" class="submit_btn" @click="submitForm('registerForm')">注 册</el-button>
             </el-form-item>
         </el-form>
     </div>
@@ -15,19 +22,60 @@
     export default {
         name: "register",
         components: {},
-        data(){
+        //数据模型,校验等
+        data() {
+            var validatePass = (rule, value, callback) => {
+                if (value !== this.registerUser.password) {
+                    callback(new Error("两次输入密码不一致!"));
+                } else {
+                    callback();
+                }
+            };
             return {
                 registerUser: {
                     username: "",
                     password: "",
+                    repeatPassword: ""
+                },
+                rules: {
+                    username: [
+                        {required: true, message: "用户名不能为空", trigger: "change"},
+                        {min: 2, max: 30, message: "长度在 2 到 30 个字符", trigger: "blur"}
+                    ],
+                    repeatPassword: [
+                        //自定义校验, 重复密码匹配校验
+                        {validator: validatePass, trigger: "blur"}
+                    ]
+
                 }
+            }
+        },
+        methods: {
+            submitForm(formName) {
+                this.$refs[formName].validate(valid => {
+                    //提交注册时,去校验表单字段, valid是校验结果
+                    if (valid) {
+                        alert("表单校验通过");
+                    } else {
+                        //校验失败
+                        console.log("提交表单: 校验失败");
+                        return false;
+                    }
+                });
             }
         }
     }
 </script>
 
 <style scoped>
-.register .registerForm{
+    .register .registerForm {
+        position: absolute;
+        left: 40%;
+        top: 40%;
+        align-content: center;
+    }
 
-}
+    .register .registerForm .text-input {
+        max-width: 200px;
+    }
 </style>
---
+++ vue.config.js
@@ -0,0 +1,8 @@
+// vue.config.js
+module.exports = {
+    // 选项...
+    publicPath: './',
+    outputDir: 'dist',
+    assetsDir: 'static',
+    indexPath: 'index.html'
+}
