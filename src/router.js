5dcd38bb7856900f24e98a62895bbaeb85b7ee67
suveng <suveng@163.com>
2019-12-02 14:18:52 +0800

    加入bootstrap以及图标库

--- src/router.js
+++ src/router.js
@@ -3,30 +3,37 @@ import Router from 'vue-router'
 import Index from "./views/Index";
 import NotFound from "./views/NotFound"
 import Register from "./views/Register";
+import Nav from "./views/Nav";
+
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      redirect: Index
-    },
-    {
-      path: '/index',
-      name: 'index',
-      component: Index
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: Register
-    },
-    {
-      path: '*',
-      name: 'notFound',
-      component: NotFound
-    }
-  ]
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            redirect: Index
+        },
+        {
+            path: '/index',
+            name: 'index',
+            component: Index
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: Register
+        },
+        {
+            path: "/nav",
+            name: "nav",
+            component: Nav
+        },
+        {
+            path: '*',
+            name: 'notFound',
+            component: NotFound
+        }
+    ]
 })
---
+++ src/views/Nav.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="nav">
+        <button type="button" class="btn btn-primary">Primary</button>
+        <br>
+        <span class="oi oi-copywriting"></span><br>
+        <span class="oi oi-caret-top"></span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Nav.vue"
+    }
+</script>
+<style scoped>
+    /*引入bootstrap样式*/
+    @import "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css";
+    /*引入bootstarp 图标 样式*/
+    @import "../assets/css/open-iconic-bootstrap.min.css";
+</style>
