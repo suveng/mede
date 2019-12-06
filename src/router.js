f499d9c01aeb3bce727fdd5c86ffbe5ccead90f9
suveng <suveng@163.com>
2019-12-06 11:02:32 +0800

    涛哥牛批

--- src/router.js
+++ src/router.js
@@ -8,7 +8,7 @@ import Nav from "./views/Nav";
 Vue.use(Router);
 
 export default new Router({
-    mode: 'history',
+    // mode: 'history',
     base: process.env.BASE_URL,
     routes: [
         {
